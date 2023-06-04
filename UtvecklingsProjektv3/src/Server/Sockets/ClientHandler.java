d32772a5d693dda652d186693e1bb93671037dd8
HampusHillborg <hampushillborg@gmail.com>
2023-06-04 11:56:26 +0200

    Added docs

--- UtvecklingsProjektv3/src/Server/Sockets/ClientHandler.java
+++ UtvecklingsProjektv3/src/Server/Sockets/ClientHandler.java
@@ -1,4 +1,9 @@
+/**
+ * The `ClientHandler` class represents a handler for a client connected to the server.
+ * It manages the communication between the client and the server.
+ */
 package Server.Sockets;
+
 import Entity.Buffer;
 import Entity.Message;
 import Entity.User;
@@ -12,6 +17,12 @@ public class ClientHandler {
     private User user;
     private Server server;
 
+    /**
+     * Constructs a new `ClientHandler` object.
+     *
+     * @param socket the socket associated with the client connection
+     * @param server the server instance
+     */
     public ClientHandler(Socket socket, Server server) {
         this.socket = socket;
         this.server = server;
@@ -20,58 +31,103 @@ public class ClientHandler {
         new MessageSender().start();
     }
 
+    /**
+     * Returns the user associated with this client handler.
+     *
+     * @return the user object
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Adds a new message to the object buffer to be sent to the client.
+     *
+     * @param message the message to be sent
+     */
     public void addMessage(Message message) {
         objectBuffer.put(message);
     }
 
+    /**
+     * Notifies the client handler about a new server update.
+     *
+     * @param update the server update object
+     */
     public void newServerUpdate(Object update) {
         objectBuffer.put(update);
     }
 
+    /**
+     * The `MessageReceiver` class is responsible for receiving messages from the client.
+     */
     private class MessageReceiver extends Thread {
         private ObjectInputStream ois;
         private ClientHandler clientHandler;
 
+        /**
+         * Constructs a new `MessageReceiver` object.
+         *
+         * @param clientHandler the client handler object
+         */
         public MessageReceiver(ClientHandler clientHandler) {
             this.clientHandler = clientHandler;
         }
 
+        /**
+         * Starts the message receiving process.
+         */
         public void run() {
             try {
                 ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+
+                // Read the User object sent by the client
                 user = (User) ois.readObject();
-                System.out.println(user.getUsername() + " anslöt sig.");
+
+                // Notify the server that a client has connected
+                System.out.println(user.getUsername() + " connected.");
                 server.clientConnected(clientHandler);
 
                 while (true) {
+                    // Receive the next object from the client
                     Object obj = ois.readObject();
 
                     if (obj instanceof Message) {
+                        // If the received object is a Message, process it accordingly
                         Message msg = (Message) obj;
+
                         if (msg.getText().equals("//disconnect")) {
+                            // If the message text is "//disconnect", the client wants to disconnect
                             server.clientDisconnected(user);
-                            System.out.println("En klient disconnetar!");
+                            System.out.println("A client disconnected!");
                         } else {
+                            // Otherwise, it's a regular message to be sent to other clients
                             server.sendMessage(msg);
                             saveMessageToFile(msg);
                         }
                     }
 
                     if (obj instanceof User) {
+                        // If the received object is a User, update the active client user in the server
                         User user = (User) obj;
                         System.out.println(user.getContacts());
                         server.updateActiveClientUser(user, clientHandler);
                     }
                 }
-            } catch (Exception e) {
+            } catch (IOException e) {
+                // Handle IO exceptions
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                // Handle class not found exceptions
                 e.printStackTrace();
             }
         }
 
+        /**
+         * Saves the received message to a file.
+         *
+         * @param message the message to be saved
+         */
         private void saveMessageToFile(Message message) {
             try (FileWriter writer = new FileWriter("messages.txt", true)) {
                 String formattedMessage = String.format("[%s] %s: %s\n", message.getDateTime(), message.getSender(), message.getText());
@@ -82,24 +138,32 @@ public class ClientHandler {
         }
     }
 
+    /**
+     * The `MessageSender` class is responsible for sending messages to the client.
+     */
     private class MessageSender extends Thread {
         private ObjectOutputStream oos;
 
+        /**
+         * Starts the message sending process.
+         */
         public void run() {
             try {
                 oos = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
                 while (true) {
+                    // Retrieve the next object from the object buffer
                     Object obj = objectBuffer.get();
+                    // If an object is available, send it to the client
                     if (obj != null) {
                         oos.writeObject(obj);
                         oos.flush();
-                        System.out.println("Skickat något till klient");
+                        System.out.println("Sent something to the client");
                     }
                 }
+                // Handle IO exceptions
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 }
-
--- UtvecklingsProjektv3/src/Server/Sockets/Server.java
+++ UtvecklingsProjektv3/src/Server/Sockets/Server.java
@@ -1,3 +1,7 @@
+/**
+ * The `Server` class represents a server that handles client connections and message communication.
+ * It manages the online users, contact lists, and message sending/receiving.
+ */
 package Server.Sockets;
 
 import Entity.*;
@@ -22,6 +26,11 @@ public class Server extends Thread{
     private ClientHandler latestClient;
     private ServerUpdate serverUpdate = null;
 
+    /**
+     * Constructs a new `Server` object.
+     * Initializes the online user and contact list data structures.
+     * Starts the server.
+     */
     public Server(){
         this.trafficViewer = new TrafficViewer();
         onlineUsers = new HashMap<>();
@@ -30,6 +39,10 @@ public class Server extends Thread{
         start();
     }
 
+    /**
+     * Starts the server and listens for client connections.
+     * Creates a new `ClientHandler` for each connected client.
+     */
     @Override
     public void run() {
         System.out.println("Server started");
@@ -48,6 +61,12 @@ public class Server extends Thread{
         }
     }
 
+    /**
+     * Notifies the server that a client has connected.
+     * Adds the client to the online users and sends stored messages and contact list updates.
+     *
+     * @param clientHandler the client handler associated with the connected client
+     */
     public void clientConnected(ClientHandler clientHandler) {
         User user = clientHandler.getUser();
         onlineUsers.put(user, clientHandler);
@@ -66,10 +85,19 @@ public class Server extends Thread{
         createAndSendServerUpdate();
     }
 
+    /**
+     * Returns the latest server update.
+     *
+     * @return the latest server update
+     */
     private ServerUpdate getUpdate() {
         return serverUpdate;
     }
 
+    /**
+     * Creates a server update object and sends it to all connected clients.
+     * The server update includes the list of connected users and the new user connected.
+     */
     private void createAndSendServerUpdate(){
         ServerUpdate serverUpdate = new ServerUpdate();
 
@@ -82,33 +110,57 @@ public class Server extends Thread{
         onlineUsers.forEach(((user, clientConnection) -> clientConnection.newServerUpdate(serverUpdate)));
     }
 
+    /**
+     * Notifies the server that a client has disconnected.
+     * Removes the client from the online users and sends a server update to all connected clients.
+     *
+     * @param user the user associated with the disconnected client
+     */
     public void clientDisconnected(User user) {
         onlineUsers.remove(user);
         System.out.println(user.getUsername() + " disconnected.. :(");
         createAndSendServerUpdate();
     }
 
+    /**
+     * Sends a message to the specified recipients.
+     * If a recipient is online, the message is sent directly.
+     * Otherwise, the message is stored for the recipient until they come online.
+     *
+     * @param msg the message to be sent
+     */
     public void sendMessage(Message msg) {
         ArrayList<User> recipients = msg.getRecipients();
-        System.out.println("Skickar msg:s");
+        System.out.println("Sending messages");
 
         for(int i = 0; i < recipients.size(); i++){
             User receiver = recipients.get(i);
             if(onlineUsers.containsKey(receiver)){
                 onlineUsers.get(receiver).addMessage(msg);
-
-            }else{
+            } else {
                 storedMessages.put(receiver, msg);
             }
         }
     }
 
+    /**
+     * Updates the active client user with the provided user object and client handler.
+     * Adds the user to the online users and updates the contact list for the user.
+     *
+     * @param user           the user to be updated
+     * @param clientHandler  the client handler associated with the user
+     */
     public void updateActiveClientUser(User user, ClientHandler clientHandler) {
-        System.out.println("Uppdaterade kontakterna för " + user.getUsername() + " med kontakterna " + user.getContacts());
+        System.out.println("Updated contacts for " + user.getUsername() + " with contacts " + user.getContacts());
         onlineUsers.put(user, clientHandler);
         contactList.put(user, user.getContacts());
     }
 
+    /**
+     * The entry point of the server application.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         new Server();
     }
